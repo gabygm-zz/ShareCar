3e70c058315506cbcdb2a71defa0598cd1a8c98f
Gaby G <paogaby0803@gmail.com>
2020-08-09 20:17:32 -0500

    validate when are not a numbers

--- src/models/trip.js
+++ src/models/trip.js
@@ -1,10 +1,27 @@
-const Trip = function(price, passengers){
+const PropTypes = require('prop-types')
+
+const Trip = function(price, passengers, date = new Date()){
     this.price = price;
     this.passengers = passengers;
+    this.date = date;
 }
 
 Trip.prototype.calculateAmount = function(){
-    return this.price * this.passengers;
+    try {
+        if(isNaN(this.price) || isNaN(this.passengers)){
+            throw 'Price must be a number'
+        }
+        return this.price * this.passengers;
+       
+    } catch(e) {
+       throw e
+    }
+}
+
+Trip.propTypes = {
+    price : PropTypes.string,
+    passengers : PropTypes.number,
+    date: PropTypes.date
 }
 
 module.exports = Trip;
