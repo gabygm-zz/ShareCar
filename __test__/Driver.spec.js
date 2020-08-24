0389bca641e0bd8674d926f700524baafd13a3d4
Gaby G <paogaby0803@gmail.com>
2020-08-23 20:31:17 -0500

    refactor test driver code duplicate

--- __test__/Driver.spec.js
+++ __test__/Driver.spec.js
@@ -2,16 +2,17 @@ const Driver = require('../src/models/driver')
 const moment = require('moment');
 
 describe('Drive Service', function() {
+  const driver = new Driver('Gaby', 'G', 'Loja', ['Saturday', 'Wednesday'])
+
   test('Should return true if today is days not available', () =>{
-    const drive = new Driver('Gaby', 'G', 'Loja', ['Saturday', 'Wednesday'])
     const today = () => moment.today
-    const isAvailable = drive.isAvailable(today);
+    const isAvailable = driver.isAvailable(today);
     expect(isAvailable).toBe(true)
   })
+
   test('Should return false if Saturday  is a day not available', () =>{
-    const drive = new Driver('Gaby', 'G', 'Loja', ['Saturday', 'Wednesday'])
     const sunday = () => 'Saturday'
-    const isAvailable = drive.isAvailable(sunday);
+    const isAvailable = driver.isAvailable(sunday);
     expect(isAvailable).toBe(false)
   })
 })
--- __test__/TripTest.spec.js
+++ __test__/TripTest.spec.js
@@ -1,23 +1,7 @@
 const Trip = require('../src/models/trip')
 const Car = require('../src/models/car')
 
-beforeEach(() => {
-
-});
-
-afterEach(() => {
-  
-});
-
-beforeAll(() => {
-
-});
-
-afterAll(() => {
-
-});
-
-describe('Trip service', function() {
+describe('Trip cost', function() {
   test('should return total amount trip given price and number passengers', () =>{
     const trip = new Trip(2.5,2)
   
@@ -28,15 +12,16 @@ describe('Trip service', function() {
 
   test('Should return an error message given a price as a string parameter', () => {
     const trip = new Trip('number', 2)
-
     expect(() => {
       trip.calculateAmount()
     }).toThrow('Price must be a number')
   })
+})
 
+describe('Trip services', function() {
   test('Should return true if car accept pets', () => {
     const car = new Car('chevrolet', '4', true)
-    const trip = new Trip('number', 2, new Date(), car)
+    const trip = new Trip(2, 2, new Date(), car)
     expect(trip.acceptPets()).toBeTruthy()
   })
 })
